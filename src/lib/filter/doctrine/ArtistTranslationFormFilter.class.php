674ccafcf4049a2f1670f3d6cdb74bd0bf46f76c
Tristan Rivoallan <tristan@rivoallan.net>
2012-01-18 23:35:11 +0100

    [backend/artist] I18n stub

---
+++ src/lib/filter/doctrine/ArtistTranslationFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ArtistTranslation filter form.
+ *
+ * @package    daheardit-records.net
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ArtistTranslationFormFilter extends BaseArtistTranslationFormFilter
+{
+  public function configure()
+  {
+  }
+}
--- src/lib/form/doctrine/ArtistTranslationForm.class.php
+++ src/lib/form/doctrine/ArtistTranslationForm.class.php
@@ -1,17 +1,16 @@
 <?php
 
 /**
- * Artist form.
+ * ArtistTranslation form.
  *
  * @package    daheardit-records.net
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class ArtistForm extends BaseArtistForm
+class ArtistTranslationForm extends BaseArtistTranslationForm
 {
   public function configure()
   {
-  	$this->embedI18n(array('fr', 'en'));
   }
 }
